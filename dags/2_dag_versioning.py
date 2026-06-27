ee282a9906502b990290d672b1a9e643465d5b03
cong205 <nguyendinhcong20112005@gmail.com>
2026-06-27 15:59:31 +0700

    add dag_version

---
+++ dags/2_dag_versioning.py
@@ -0,0 +1,21 @@
+from airflow.sdk import dag, task
+
+@dag(
+    dag_id="version_dag"
+)
+def version_dag():
+    @task.python
+    def first_task():
+        print("This is the first task")
+    
+    @task.python
+    def second_task():
+        print("This is the second task")
+        
+    @task.python
+    def third_task():
+        print("This is the third task") 
+        
+    first_task() >> second_task() >> third_task()
+
+version_dag()
